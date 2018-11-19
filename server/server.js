342b4a126ee3115becb9a5694c71106848080997
Andrew Heller <andrewhheller@gmail.com>
2018-11-19 14:12:23 -0500

    add basic get route test

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const app = require('./app');
+
+
+const PORT = 3000 || process.env.PORT;
+
+
+
+app.listen(PORT, () => {
+  console.log(`listening on port... ${ PORT }`)
+})
---
+++ test/test.js
@@ -0,0 +1,19 @@
+const app = require('supertest')(require('../server/app'));
+const expect = require('chai').expect;
+
+
+describe('our app', () => {
+
+  describe('get route', () => {
+
+    it('returns hello world', () => {
+      return app.get('/')
+        .expect(200)
+        .then(response => {
+          expect(response.text).to.equal('hello world')
+        })
+    })
+
+  })
+
+})
